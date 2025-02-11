22b1793e9d849b03bc6b5eec06595fa7e3c8e6c3
Rafael Pototsky Lourenço <rafaelpototskylourenco10@gmail.com>
2025-02-11 18:33:36 -0300

    Refactor input handling for target sum calculation and improve user prompts

--- Atividade 2/app.js
+++ Atividade 2/app.js
@@ -1,4 +1,4 @@
-/*Dado um array de inteiros nums e um inteiro target, retorne os índices dos dois números de modo que a soma deles sejatarget .
+/*Dado um array de inteiros nums e um inteiro target, retorne os índices dos dois números de modo que a soma deles seja target.
 
 Você pode assumir que cada entrada teria exatamente uma solução e não pode usar o mesmo elemento duas vezes.
 
@@ -7,17 +7,17 @@ Você pode retornar a resposta em qualquer ordem.
 Exemplo 1:
 
 Entrada: nums = [2,7,11,15], alvo = 9
- Saída: [0,1]
- Explicação: Como nums[0] + nums[1] == 9, retornamos [0, 1].
+Saída: [0,1]
+Explicação: Como nums[0] + nums[1] == 9, retornamos [0, 1].
 Exemplo 2:
 
 Entrada: nums = [3,2,4], alvo = 6
- Saída: [1,2]
+Saída: [1,2]
 Exemplo 3:
 
 Entrada: nums = [3,3], alvo = 6
- Saída: [0,1]
- 
+Saída: [0,1]
+
 
 Restrições:
 
@@ -26,3 +26,25 @@ Restrições:
 -109 <= target <= 109
 Existe apenas uma resposta válida.*/
 
+var nums = [], target, quantia;
+
+target = prompt("Digite o valor alvo: ");
+quantia = prompt("Digite a quantia a ser somada: ");
+
+for(i = 0; i < quantia; i++)
+{
+     nums[i] = prompt("Digite os valores a serem somados: ");
+     while(nums[i] == numsIguais)
+     {
+          nums[i] = prompt("Esse valor já foi usado! Digite um valor diferente: ");
+     }
+     var numsIguais = nums[i];
+}
+
+for(i = 0; i < quantia; i++)
+{
+     if(target < nums[i])
+     {
+          
+     }
+}
